a084b0e37239d30760db6962af6c76e5c1c860a1
Efehan Atici <efehanatici@gmail.com>
2023-05-02 21:30:46 +0300

    Return user_details from database.

--- app/routers/user.py
+++ app/routers/user.py
@@ -3,10 +3,13 @@ from typing import Annotated
 
 from fastapi import APIRouter, Depends, Cookie
 
+from app.config import settings
+from app.db.mongodb import AsyncMongoClient
 from app.utils.jwt import decode_jwt
 
 logger = logging.getLogger(__name__)
 router = APIRouter()
+mongo_db = AsyncMongoClient(settings.MONGODB_URL)
 
 
 def decode_user_token(token: Annotated[str, Cookie()]):
@@ -15,4 +18,5 @@ def decode_user_token(token: Annotated[str, Cookie()]):
 
 @router.get("/user_details", summary="Gets registered user details from database")
 async def get_user_details(user: Annotated[dict, Depends(decode_user_token)]):
-    return user
+    db_user = await mongo_db.get_user_from_osu_id(user["osu_id"])
+    return db_user
