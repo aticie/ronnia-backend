e13c0ae0febc47af5dcc8601f671f3cbcd9bffb2
aticie <efehanatici@gmail.com>
2023-05-20 13:27:02 +0200

    Change top requests endpoint, allow query parameters.

--- app/routers/requests.py
+++ app/routers/requests.py
@@ -9,29 +9,11 @@ router = APIRouter(prefix="/requests", tags=["requests"])
 mongo_db = AsyncMongoClient(settings.MONGODB_URL)
 
 
-@router.get("/beatmaps/top/daily", summary="Shows top requested beatmaps for today.")
-async def top_beatmap_requests_day(limit: int = 5, offset: int = 0):
-    time_start = datetime.datetime.today() - datetime.timedelta(days=1)
-    return await mongo_db.get_top_requested_beatmaps(
-        time_start=time_start, limit=limit, offset=offset
-    )
-
-
 @router.get(
-    "/beatmaps/top/weekly", summary="Shows top requested beatmaps for this week."
+    "/beatmaps/top", summary="Shows top requested beatmaps for the selected time."
 )
-async def top_beatmap_requests_week(limit: int = 5, offset: int = 0):
-    time_start = datetime.datetime.today() - datetime.timedelta(days=30)
-    return await mongo_db.get_top_requested_beatmaps(
-        time_start=time_start, limit=limit, offset=offset
-    )
-
-
-@router.get(
-    "/beatmaps/top/monthly", summary="Shows top requested beatmaps for this month."
-)
-async def top_beatmap_requests_month(limit: int = 5, offset: int = 0):
-    time_start = datetime.datetime.today() - datetime.timedelta(days=30)
+async def top_beatmap_requests_day(days_ago: int = 1, limit: int = 5, offset: int = 0):
+    time_start = datetime.datetime.today() - datetime.timedelta(days=days_ago)
     return await mongo_db.get_top_requested_beatmaps(
         time_start=time_start, limit=limit, offset=offset
     )
